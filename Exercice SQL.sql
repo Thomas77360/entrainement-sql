9198f75aa47d1ccf4e7fd084e396c30cbecfdf89
Thomas Bourse <thomas@macmini-1.home>
2026-04-18 18:44:39 +0200

    Ajout des réponses aux exercices SQL

--- Exercice SQL.sql
+++ Exercice SQL.sql
@@ -1,5 +1,5 @@
 /*
-Dataseito - Les bases de SQL pour débutant - Exercice  
+Dataseito - Les bases de SQL pour dï¿½butant - Exercice  
 */
 
 
@@ -8,79 +8,132 @@ Dataseito - Les bases de SQL pour débutant - Exercice
 
 
 
--- Sélectionner toutes les données présentes dans la table Vente
+-- Sï¿½lectionner toutes les donnï¿½es prï¿½sentes dans la table Vente
+USE MonEntreprise
+GO
+SELECT *
+FROM [dbo].[Vente];
+
+-- Sï¿½lectionner la colonne VenteID de la table Vente
+SELECT [VenteID]
+FROM [dbo].[Vente];
+
+-- Sï¿½lectionner les colonnes VenteID et ClientID de la table Vente
+SELECT [VenteID], [ClientID]
+FROM [dbo].[Vente];
+
+-- Limiter le nombre de rï¿½sultats retournï¿½s par la requï¿½te prï¿½cï¿½dente ï¿½ 3 
+SELECT top (3) [VenteID], [ClientID]
+FROM [dbo].[Vente];
+
+-- Sï¿½lectionner toutes les ventes relatives au CLIENT_1
+SELECT [VenteID], [ClientID], [VentesEuro]
+    FROM [dbo].[Vente]
+    WHERE [ClientID] ='CLIENT_1';
+
+-- Sï¿½lectionner toutes les ventes non relatives au CLIENT_1
+SELECT [VenteID], [ClientID], [VentesEuro]
+    FROM [dbo].[Vente]
+    WHERE [ClientID] !='CLIENT_1';
+
+-- Sï¿½lectionner toutes les ventes du produit PROD_1 relatives au CLIENT_1 
+SELECT [VenteID], [ProduitID], [ClientID], [VentesEuro]
+    FROM [dbo].[Vente]
+    WHERE [ClientID] ='CLIENT_1' AND [ProduitID] = 'PROD_1';
+
+-- Sï¿½lectionner toutes les ventes relatives au CLIENT_1 ou au CLIENT_2
+SELECT [VenteID], [ProduitID], [ClientID], [VentesEuro]
+    FROM [dbo].[Vente]
+    WHERE [ClientID] ='CLIENT_1' OR [ClientID] = 'CLIENT_2';
+
+-- Sï¿½lectionner toutes les ventes relatives au CLIENT_1, au CLIENT_2 ou au CLIENT_3
+SELECT [VenteID], [ProduitID], [ClientID], [VentesEuro]
+    FROM [dbo].[Vente]
+    WHERE [ClientID] IN ('CLIENT_1','CLIENT_2','CLIENT_3');
+
+-- Sï¿½lectionner toutes les ventes qui ne sont pas relatives au CLIENT_1, au CLIENT_2 ou au CLIENT_3
+SELECT [VenteID], [ProduitID], [ClientID], [VentesEuro]
+    FROM [dbo].[Vente]
+    WHERE [ClientID] NOT IN ('CLIENT_1','CLIENT_2','CLIENT_3');
+
+-- Sï¿½lectionner toutes les ventes ID qui commencent par 10
+SELECT [VenteID], [ProduitID], [ClientID], [VentesEuro]
+    FROM [dbo].[Vente]
+    WHERE [VenteID] LIKE '10%';
+
+-- Sï¿½lectionner toutes les ventes qui finissent par 10
+SELECT [VenteID], [ProduitID], [ClientID], [VentesEuro]
+    FROM [dbo].[Vente]
+    WHERE [VenteID] LIKE '%10';
+
+-- Sï¿½lectionner toutes les ventes qui contiennent 10
+SELECT [VenteID], [ProduitID], [ClientID], [VentesEuro]
+    FROM [dbo].[Vente]
+    WHERE [VenteID] LIKE '%10%';
+
+-- Sï¿½lectionner toutes les ventes du produit PROD_1 relatives au CLIENT_1 triï¿½es par le montant des ventes de maniï¿½re dï¿½croissante
+SELECT [VenteID], [ProduitID], [ClientID], [VentesEuro]
+    FROM [dbo].[Vente]
+    WHERE [ProduitID] = 'PROD_1' AND [ClientID] = 'CLIENT_1'
+    ORDER BY [ProduitID] DESC;
+
+-- Sï¿½lectionner la liste des clients uniques
+SELECT DISTINCT [ClientID]
+    FROM [dbo].[Vente]
+    ORDER BY [ClientID] DESC;
 
 
--- Sélectionner la colonne VenteID de la table Vente
 
-
--- Sélectionner les colonnes VenteID et ClientID de la table Vente
-
-
--- Limiter le nombre de résultats retournés par la requête précédente à 3 
-
-
--- Sélectionner toutes les ventes relatives au CLIENT_1
-
-
--- Sélectionner toutes les ventes non relatives au CLIENT_1
-
-
--- Sélectionner toutes les ventes du produit PROD_1 relatives au CLIENT_1 
-
-
--- Sélectionner toutes les ventes relatives au CLIENT_1 ou au CLIENT_2
-
-
--- Sélectionner toutes les ventes relatives au CLIENT_1, au CLIENT_2 ou au CLIENT_3
-
-
--- Sélectionner toutes les ventes qui ne sont pas relatives au CLIENT_1, au CLIENT_2 ou au CLIENT_3
-
-
--- Sélectionner toutes les ventes ID qui commencent par 10
-
-
--- Sélectionner toutes les ventes qui finissent par 10
-
-
--- Sélectionner toutes les ventes qui contiennent 10
-
-
--- Sélectionner toutes les ventes du produit PROD_1 relatives au CLIENT_1 triées par le montant des ventes de manière décroissante
-
-
--- Sélectionner la liste des clients uniques
-
-
-
----------------------------------------------------------------------------- LES AGRÉGATIONS ------------------------------------------------------------------------
+---------------------------------------------------------------------------- LES AGRï¿½GATIONS ------------------------------------------------------------------------
 
 
 
 -- Compter le nombre d'enregistrement de la table Vente
-
+SELECT COUNT (*)
+    FROM [dbo].[Vente];
 
 -- Calculer le total des ventes pour chaque client dans une colonne TotalCA
-
+SELECT [ClientID], SUM([VentesEuro]) as TotalCA
+    FROM [dbo].[Vente]
+    GROUP BY [ClientID]
+    ORDER BY TotalCA DESC;
 
 -- Calculer le total des ventes pour chaque client et produit
-
+SELECT [ClientID],[ProduitID], SUM([VentesEuro]) as TotalCA
+    FROM [dbo].[Vente]
+    GROUP BY [ClientID],[ProduitID]
+    ORDER BY TotalCA DESC; 
 
 -- Calculer le montant moyen des ventes pour chaque client
-
+SELECT [ClientID],[ProduitID], AVG([VentesEuro]) as TotalCA
+    FROM [dbo].[Vente]
+    GROUP BY [ClientID],[ProduitID]
+    ORDER BY TotalCA DESC; 
 
 -- Calculer le nombre de ventes pour chaque client
+SELECT [ClientID],[ProduitID], COUNT([VentesEuro]) as TotalCA
+    FROM [dbo].[Vente]
+    GROUP BY [ClientID],[ProduitID]
+    ORDER BY TotalCA DESC; 
 
+-- Sï¿½lectionner la vente la plus ï¿½levï¿½e pour chaque client 
+SELECT [ClientID],[ProduitID], MAX([VentesEuro]) as TotalCA
+    FROM [dbo].[Vente]
+    GROUP BY [ClientID],[ProduitID]
+    ORDER BY TotalCA DESC; 
 
--- Sélectionner la vente la plus élevée pour chaque client 
+-- Sï¿½lectionner la vente la plus faible pour chaque client 
+SELECT [ClientID],[ProduitID], MIN([VentesEuro]) as TotalCA
+    FROM [dbo].[Vente]
+    GROUP BY [ClientID],[ProduitID]
+    ORDER BY TotalCA DESC; 
 
-
--- Sélectionner la vente la plus faible pour chaque client 
-
-
--- Calculer le total des ventes par client pour les clients ayant un chiffre d'affaire supérieur à 1 000 000 €
-
+-- Calculer le total des ventes par client pour les clients ayant un chiffre d'affaire supï¿½rieur ï¿½ 1 000 000 ï¿½
+SELECT [ClientID], SUM([VentesEuro]) as TotalCA
+    FROM [dbo].[Vente]
+    GROUP BY [ClientID]
+    HAVING SUM([VentesEuro]) > 1000000
+    ORDER BY TotalCA DESC; 
 
 
 
@@ -88,41 +141,81 @@ Dataseito - Les bases de SQL pour débutant - Exercice
 
 
 
--- Se positionner sur la base de données [Jointure]
+-- Se positionner sur la base de donnï¿½es [Jointure]
+USE Jointure
+GO
 
+SELECT *
+FROM [dbo].[TableA];
+
+SELECT *
+FROM [dbo].[TableB]
+
+SELECT * 
+FROM [dbo].[TableC]
+
+SELECT *
+FROM [dbo].[TableD];
 
 -- Faire une jointure de type LEFT JOIN
-
+SELECT [TableA_ID],[TableB_ID]
+FROM [dbo].[TableA]
+LEFT JOIN [dbo].[TableB] ON [dbo].[TableA].TableA_ID = [dbo].[TableB].TableB_ID;
 
 -- Faire une jointure de type RIGHT JOIN
-
+SELECT [TableA_ID],[TableB_ID]
+FROM [dbo].[TableA]
+RIGHT JOIN [dbo].[TableB] ON [dbo].[TableA].TableA_ID = [dbo].[TableB].TableB_ID;
 
 -- Faire une jointure de type INNER JOIN
-
+SELECT [TableA_ID],[TableB_ID]
+FROM [dbo].[TableA]
+INNER JOIN [dbo].[TableB] ON [dbo].[TableA].TableA_ID = [dbo].[TableB].TableB_ID;
 
 -- Faire une jointure de type OUTER JOIN
-
+SELECT [TableA_ID],[TableB_ID]
+FROM [dbo].[TableA]
+full  OUTER JOIN [dbo].[TableB] ON [dbo].[TableA].TableA_ID = [dbo].[TableB].TableB_ID;
 
 -- Faire une jointure de type CROSS JOIN
+SELECT [TableA_ID],[TableB_ID]
+FROM [dbo].[TableA]
+CROSS JOIN [dbo].[TableB];
+
+-- Se positionner sur la base de donnï¿½es [MonEntreprise]
+USE [MonEntreprise]
+GO
+SELECT *
+FROM [dbo].[Vente];
+
+-- Rï¿½cupï¿½rer les noms des clients dans la table Client avec le montant des ventes liï¿½es
+SELECT [dbo].[Client].[ClientID], [dbo].[Vente].[ClientID], [dbo].[Vente].[VentesEuro]
+FROM [dbo].[Client]
+LEFT JOIN [dbo].[Vente] ON [dbo].[Client].ClientID = [dbo].[Vente].[ClientID]
+
+-- Se positionner sur la base de donnï¿½es [Jointure] ï¿½ l'aide du langage SQL
+USE [Jointure]
+GO
+SELECT *
+FROM [dbo].[TableA]
 
 
--- Se positionner sur la base de données [MonEntreprise]
+-- Concatï¿½ner les enregistrements des tables "TableC" et "TableD" et suppression des doublons avec UNION 
+SELECT ID, Prenom
+FROM [dbo].[TableC]
+UNION
+SELECT ID, Prenom
+FROM [dbo].[TableD]
 
-
--- Récupérer les noms des clients dans la table Client avec le montant des ventes liées
-
-
--- Se positionner sur la base de données [Jointure] à l'aide du langage SQL
-
-
--- Concaténer les enregistrements des tables "TableC" et "TableD" et suppression des doublons avec UNION 
-
-
--- Concaténer les enregistrements des tables "TableC" et "TableD" avec UNION ALL
-
+-- Concatï¿½ner les enregistrements des tables "TableC" et "TableD" avec UNION ALL
+SELECT ID, Prenom
+FROM [dbo].[TableC]
+UNION ALL
+SELECT ID, Prenom
+FROM [dbo].[TableD]
 
 /*
- Bravo, vous êtes arrivé au bout :)
+ Bravo, vous ï¿½tes arrivï¿½ au bout :)
 */
 
 
